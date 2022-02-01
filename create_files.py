ad64e8ea01f6acd90caff054e1c3abae4d32a375
Sergunin_Evgeniy <mckrei@ya.ru>
2022-02-01 22:48:26 +0300

    branch lesson2

---
+++ create_files.py
@@ -0,0 +1,18 @@
+
+def create_n_files(number_files):
+	'''
+	Принимает количество файлов которое нужно создать, 
+	путь до папки и текст внутри файла изменить можно внутри функции.
+	'''
+	ind = 1
+	while number_files >= ind:
+		try:
+			with open(f"lesson_2\Task_{ind}.py", "w") as file_obj: #путь до папки и имя файла-ов!
+				file_obj.write(f"'''\nText task {ind} \n'''\n") #Текст в файлах, можно удалить
+			print(f'Файл №{ind} создан!')
+		
+			ind += 1 
+		except IOError:
+			print('Ошибка ввода-вывода!')	
+create_n_files(9)
+
